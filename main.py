3c09de36ddcb1303e5b5f232fa0b562dffaab2a3
dylanrattai <817682@mystma.org>
2022-09-21 14:19:13 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-#WRITE YOUR CODE IN THIS FILE
+def greater10(x, y):
+    if 10 - x < 10 - y:
+        return x
+    elif 10 - x > 10 - y:
+        return y
+    else:
+        return 0
+
+print(greater10(4,4))
